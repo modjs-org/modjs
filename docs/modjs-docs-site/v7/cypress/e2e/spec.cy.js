9f9446934137f00804c1a69e7d9b62106f99dade
Prashan Pudasaini <prashan@Prashans-MacBook-Pro.local>
2024-08-05 13:54:00 -0500

    update scripts and config e2e test for docs-site

---
+++ docs/modjs-docs-site/v7/cypress/e2e/spec.cy.js
@@ -0,0 +1,7 @@
+describe('Mod.js Documentation Site', () => {
+    context('Find Broken Links', () => {
+        it('should check for broken links', () => {
+            cy.visit('http://localhost:3000/')
+        })
+    })
+})
