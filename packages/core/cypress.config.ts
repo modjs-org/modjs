7e200c09d3557d52494384c7a21bc9327b03c5e1
Prashan Pudasaini <prashan@Prashans-MacBook-Pro.local>
2024-08-24 19:22:35 -0500

    configure e2e test setup with cypress

--- packages/core/cypress.config.ts
+++ packages/core/cypress.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-    viewportWidth: 769, //CAUTION: A lot of tests that depends on viewport size will fail if this is changed. Modify this in each test cases/suite instead whenever necessary
-    viewportHeight: 600,
-    component: {
-        specPattern: 'src/**/*.spec.{ts,tsx}',
-        devServer: {
-            framework: 'react',
-            bundler: 'webpack',
-        },
+  //CAUTION: A lot of tests that depends on viewport size will fail if this is changed. Modify this in each test cases/suite instead whenever necessary
+  viewportWidth: 769,
+
+  viewportHeight: 600,
+
+  component: {
+    specPattern: "src/**/*.spec.{ts,tsx}",
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
     },
-})
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
